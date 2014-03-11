857b8b9d22271af90ceca4c94137a471fbbccf34
Daniel + Jorge <Daniel+Jorge@appacademy.io>
2014-03-11 13:51:48 -0400

    King class inherits from SteppingPiece

--- sliding_piece.rb
+++ sliding_piece.rb
@@ -2,30 +2,60 @@ require_relative 'chess_files'
 
 class SlidingPiece < Piece
 
-  def initialize(position)
-    super(position) #is this the way it should inherit?
-
+  def initialize(position, board)
+    super(position, board)
   end
 
-  def moves
+  def moves(start_position, range = 1)
+    moves = []
+    row, col = start_position
 
+    1.upto(range) do |mult|
+      self.class.move_dirs.each do |(row_shift, col_shift)|
+        moves << [row + (row_shift * mult), col + (col_shift * mult)]
+      end
+    end
+    moves.select{|move| super.include?(move)}
   end
-
-
-end
-
-class King < SlidingPiece
+  end
 
 end
 
 class Queen < SlidingPiece
 
+  def initialize(position, board)
+    super(position, board)
+  end
+
+  def self.move_dirs
+    [
+      [1, -1],
+      #[2, -2],
+
+      [1, 0],
+      [1, 1],
+      [0, 1],
+      [-1, 1],
+      [-1, 0],
+      [-1, -1],
+      [0, -1]
+    ]
+  end
+
 end
 
 class Rook < SlidingPiece
 
+  def initialize(position, board)
+    super(position, board)
+  end
+
 end
 
 class Bishop < SlidingPiece
+
+  def initialize(position, board)
+    super(position, board)
+  end
 
 end
--- stepping_piece.rb
+++ stepping_piece.rb
@@ -2,24 +2,95 @@ require_relative 'chess_files'
 
 class SteppingPiece < Piece
 
-  def initialize(position)
-    super(position) #is this the way it should inherit?
-
+  def initialize(position, board)
+    super(position, board)
   end
 
-  def moves
-    super
-  end
+  def moves(start_position, deltas)
+    moves = []
+    row, col = start_position
 
+    deltas.each do |(row_shift, col_shift)|
+      moves << [row + row_shift, col + col_shift]
+    end
+
+    moves.select{|move| super.include?(move)}
+
+  end
 
 end
 
 
 class Knight < SteppingPiece
 
+  KNIGHT_DELTAS = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2]
+  ]
+
+  def initialize(position, board)
+    super(position, board)
+  end
+
+  def moves(start_position, KNIGHT_DELTAS)
+    super
+  end
+
+end
+
+class King < SteppingPiece
+
+  KING_DELTAS = [
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+    [-1, 0],
+    [-1, -1],
+    [0, -1]
+  ]
+
+  def initialize(position, board)
+    super(position, board)
+  end
+
+  def moves(start_position, KING_DELTAS)
+    super
+  end
+
 end
 
 class Pawn < SteppingPiece
+  # Special piece class, can't move backwards.
+  # Will need to know which player is accessing this class
+  # to choose the direction of DELTAS.
+  # Currently we have DELTAS set for the player moving from
+  # the top of the board downwards.
+  # For the other player, multiply the row coordinate by -1.
+
+  DELTAS = [
+    [1, 0],
+    [2, 0],
+    [1, 1],
+    [1, -1]
+  ]
+
+  def initialize(position, board)
+    super(position, board)
+  end
+
+  def moves
+
+
+
+  end
 
 end
 
